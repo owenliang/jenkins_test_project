543de9604fa199a022239d24b0f49483f4c31a0c
梁栋 <owenliang1990@gmail.com>
2018-12-05 16:30:25 +0800

    update

---
+++ build.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+
+# 该PHP项目最终要部署到/data/webroot/phpsrc/test下面
+rm -rf deployment
+
+# 代码
+mkdir -p deployment/code
+cp index.php deployment/code
+
+# 部署脚本
+cp deploy.sh deployment/
+
+# 打包目录
+cd deployment
+tar czvf ../deploy.tar.gz *
+cd -
+
+# 删除临时目录
+rm -rf deployment
---
+++ deploy.sh
@@ -0,0 +1,4 @@
+#!/bin/bash
+
+# 代码部署到test目录
+cp -r code /data/webroot/phpsrc/test
